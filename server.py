782cffb29ce099361ea6e3ec1729cfdc34c26711
jitlada <tang2538a@gmail.com>
2021-07-23 13:24:42 +0700

    edit server

--- server.py
+++ server.py
@@ -37,5 +37,5 @@ class Server:
         return 'Hello, World!'
 
     if __name__ == '__main__':
-        app.run(debug=True)
-        # app.run(debug=True, host='0.0.0.0', port=5000)
+        # app.run(debug=True)
+        app.run(debug=True, host='0.0.0.0', port=5008)
