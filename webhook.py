977fbc84895111c660e2d71d5703b97ff7d22073
jitlada <tang2538a@gmail.com>
2021-07-27 14:57:54 +0700

    edit webhook

--- webhook.py
+++ webhook.py
@@ -67,28 +67,28 @@ class Webhook(Resource):
     #         'result': [{'onechat_token': onechat_token[1]}]
     #     }, 200
 
-    # def menu_send(self, one_id):
-    #     TAG = "menu_send:"
-    #     web_vue_url1 = "http://onesmartaccess.ddns.net:8081"
-    #     msg = "ให้ช่วยอะไรดี"
-    #     payload = [
-    #                 {
-    #                     "label": "การเข้าพื้นที่ของคุณ",
-    #                     "type": "text",
-    #                     "message": "ดูการเข้างานของฉัน",
-    #                     "payload": "my_rec"
-    #                 }
-    #             ]
-    #     if(self.is_admin(one_id)):
-    #         payload.append({
-    #                     "label": "Admin",
-    #                     "type": "link",
-    #                     "url": web_vue_url1,
-    #                     "sign": "false",
-    #                     "onechat_token": "true"
-    #                 })
-    #     res = self.send_quick_reply(one_id, msg, payload)
-    #     print(TAG, "res=", res)
+    def menu_send(self, one_id):
+        TAG = "menu_send:"
+        web_vue_url1 = "http://onesmartaccess.ddns.net:8081"
+        msg = "ให้ช่วยอะไรดี"
+        payload = [
+            {
+                "label": "การเข้าพื้นที่ของคุณ",
+                "type": "text",
+                        "message": "ดูการเข้างานของฉัน",
+                        "payload": "my_rec"
+            }
+        ]
+        if(self.is_admin(one_id)):
+            payload.append({
+                "label": "Admin",
+                "type": "link",
+                        "url": web_vue_url1,
+                        "sign": "false",
+                        "onechat_token": "true"
+            })
+        res = self.send_quick_reply(one_id, msg, payload)
+        print(TAG, "res=", res)
 
     def get_device(self, one_id):
         TAG = "get_device:"
@@ -98,18 +98,15 @@ class Webhook(Resource):
         # WHERE timeattendance.employee_code='%s' AND timeattendance.date=CURRENT_DATE""" %(one_id)
         return covid_res
 
-    # def post(self):
-    #     TAG = "Webhook:"
-    #     data = request.json
-    #     print(TAG, "data=", data)
-    #     print(TAG, request.headers)
-    #     database = Database()
-    #     module = Module()
+    def post(self):
+        TAG = "Webhook:"
+        data = request.json
+        print(TAG, "data=", data)
+        print(TAG, request.headers)
+        database = Database()
+        module = Module()
 
-    def get(self):
-
-        args = request.args
-
-        get_device = self.get_device(args)
-
-        return get_device
+    # def get(self):
+    #     args = request.args
+    #     get_device = self.get_device(args)
+    #     return get_device
